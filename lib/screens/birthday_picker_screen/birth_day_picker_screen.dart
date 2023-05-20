376d984767f0383fa2eecb6a47d67183057fc09b
Ara Periyan <periyan.ara@gmail.com>
2023-05-20 16:03:29 +0400

    Add animated assets

--- lib/screens/birthday_picker_screen/birth_day_picker_screen.dart
+++ lib/screens/birthday_picker_screen/birth_day_picker_screen.dart
@@ -27,41 +27,75 @@ class _BirthDayPickerScreenState extends State<BirthDayPickerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteModeColors.scafoldBackgroundColor,
-      body: Column(
+      body: Stack(
         children: [
-          const Spacer(flex: 4),
-          Text(
-            'Log in your date of birth',
-            style: periodTheme.body,
-          ),
-          const Spacer(flex: 1),
-          Center(
-            child: SizedBox(
-              width: 300,
-              height: 200,
-              child: CupertinoPicker(
-                diameterRatio: 4,
-                squeeze: 1.1,
-                looping: true,
-                itemExtent: 50,
-                children: date
-                    .map((date) => Center(
-                          child: Text(date, style: periodTheme.title),
-                        ))
-                    .toList(),
-                onSelectedItemChanged: (value) {},
+          Column(
+            children: [
+              const Spacer(flex: 4),
+              Text(
+                'Log in your date of birth',
+                style: periodTheme.body,
               ),
+              const Spacer(flex: 1),
+              Center(
+                child: SizedBox(
+                  width: 300,
+                  height: 200,
+                  child: CupertinoPicker(
+                    diameterRatio: 4,
+                    squeeze: 1.1,
+                    looping: true,
+                    itemExtent: 50,
+                    children: date
+                        .map((date) => Center(
+                              child: Text(date, style: periodTheme.title),
+                            ))
+                        .toList(),
+                    onSelectedItemChanged: (value) {},
+                  ),
+                ),
+              ),
+              const Spacer(flex: 3),
+              TextButton(
+                onPressed: () {},
+                child: CustomButton(
+                  iconColor: whiteModeColors.buttonColor,
+                  containerColor: whiteModeColors.primaryBackgroundColor,
+                ),
+              ),
+              const Spacer(flex: 1),
+            ],
+          ),
+          Positioned(
+            top: -220,
+            right: -220,
+            child: Image.asset(
+              MediaQuery.of(context).platformBrightness == Brightness.light
+                  ? 'assets/animations/circle_2_light.gif'
+                  : 'assets/animations/circle_2_dark.gif',
+              width: 500,
             ),
           ),
-          const Spacer(flex: 3),
-          TextButton(
-            onPressed: () {},
-            child: CustomButton(
-              iconColor: whiteModeColors.buttonColor,
-              containerColor: whiteModeColors.primaryBackgroundColor,
+          Positioned(
+            bottom: -120,
+            right: -120,
+            child: Image.asset(
+              MediaQuery.of(context).platformBrightness == Brightness.light
+                  ? 'assets/animations/circle_1_light.gif'
+                  : 'assets/animations/circle_1_dark.gif',
+              width: 300,
             ),
           ),
-          const Spacer(flex: 1),
+          Positioned(
+            bottom: 300,
+            left: 50,
+            child: Image.asset(
+              MediaQuery.of(context).platformBrightness == Brightness.light
+                  ? 'assets/animations/circle_3_light.gif'
+                  : 'assets/animations/circle_3_dark.gif',
+              width: 100,
+            ),
+          ),
         ],
       ),
     );
